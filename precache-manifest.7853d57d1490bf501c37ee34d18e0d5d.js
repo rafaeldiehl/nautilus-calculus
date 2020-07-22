3b5c06321ba0328ffe7a554385539ea98abe2ba2
Rafael Diehl <rafaeljuliani1984@gmail.com>
2020-07-21 23:03:51 -0300

    Updates

---
+++ precache-manifest.7853d57d1490bf501c37ee34d18e0d5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ca81f3e5d14b761404b9483fdcfb59c",
+    "url": "/nautilus-calculus/index.html"
+  },
+  {
+    "revision": "abaf67b2b2307ec5975e",
+    "url": "/nautilus-calculus/static/js/2.155df001.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nautilus-calculus/static/js/2.155df001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3988d2fcab9e2dc6db0b",
+    "url": "/nautilus-calculus/static/js/main.cd67c70a.chunk.js"
+  },
+  {
+    "revision": "a6db1a8fc6ae12278013",
+    "url": "/nautilus-calculus/static/js/runtime-main.eb4a3c92.js"
+  }
+]);
