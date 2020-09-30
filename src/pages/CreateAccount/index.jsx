fefc88c8fb3322786e40df7e38c831d64ab552f5
Rafael Diehl <rafaeljuliani1984@gmail.com>
2020-09-30 02:09:31 -0300

    Ajustes gerais

--- src/pages/CreateAccount/index.jsx
+++ src/pages/CreateAccount/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
+import './styles.scss';
+
 function CreateAccount() {
   return (
     <div id="create-account">
       <Helmet>
         <title>Nautilus Calculus • Criar Conta</title>
       </Helmet>
+      <h1>Em construção...</h1>
     </div>
   );
 }
