9e3fe1506361c3e132ccd0c2e0387bf9367b36cb
Rafael Diehl <rafaeljuliani1984@gmail.com>
2020-10-01 14:33:50 -0300

    Quiz em estágio inicial

--- src/components/Quiz/data.jsx
+++ src/components/Quiz/data.jsx
@@ -1,45 +1,50 @@
+import firstQuestion from '../../assets/images/moneyExercises/firstQuestion.png';
+import secondQuestion from '../../assets/images/moneyExercises/secondQuestion.png';
+import thirdQuestion from '../../assets/images/moneyExercises/thirdQuestion.png';
+import fourthQuestion from '../../assets/images/moneyExercises/fourthQuestion.png';
+import fifthQuestion from '../../assets/images/moneyExercises/fifthQuestion.png';
 import Model from '../../assets/images/model.jpg';
 
 const Data = [
   {
     id: 1,
-    image: Model,
+    image: firstQuestion,
     imageDescription: 'Lorem ipsum dolor sit amet',
-    question: 'Qual a soma da quantia acima?',
-    findAnswer: ['R$ 2,00', 'R$ 4,00', 'R$ 5,00', 'R$ 6,00'],
-    rightAnswer: 'R$ 5,00',
+    question: 'Qual a quantia total em reais somando as cédulas e moedas acima?',
+    findAnswer: ['R$ 20,75', 'R$ 21,25', 'R$ 21,75', 'R$ 22,25'],
+    rightAnswer: 'R$ 21,25',
   },
   {
     id: 2,
-    image: Model,
+    image: secondQuestion,
     imageDescription: 'Lorem ipsum dolor sit amet',
-    question: 'Qual a soma da quantia acima?',
-    findAnswer: ['R$ 2,00', 'R$ 4,00', 'R$ 5,00', 'R$ 6,00'],
-    rightAnswer: 'R$ 5,00',
+    question: 'Qual a quantia total em reais somando essas moedas?',
+    findAnswer: ['R$ 4,00', 'R$ 4,25', 'R$ 4,50', 'R$ 4,75'],
+    rightAnswer: 'R$ 4,50',
   },
   {
     id: 3,
-    image: Model,
+    image: thirdQuestion,
     imageDescription: 'Lorem ipsum dolor sit amet',
-    question: 'Qual a soma da quantia acima?',
-    findAnswer: ['R$ 2,00', 'R$ 4,00', 'R$ 5,00', 'R$ 6,00'],
-    rightAnswer: 'R$ 5,00',
+    question: 'Se você compra um produto por R$ 15,00 e entrega a quantia acima, quanto receberá de troco?',
+    findAnswer: ['R$ 2,50', 'R$ 3,00', 'R$ 3,50', 'R$ 4,00'],
+    rightAnswer: 'R$ 3,00',
   },
   {
     id: 4,
-    image: Model,
+    image: fourthQuestion,
     imageDescription: 'Lorem ipsum dolor sit amet',
-    question: 'Qual a soma da quantia acima?',
-    findAnswer: ['R$ 2,00', 'R$ 4,00', 'R$ 5,00', 'R$ 6,00'],
-    rightAnswer: 'R$ 5,00',
+    question: 'A quantia acima corresponde a quantas moedas de 25 centavos?',
+    findAnswer: ['123', '127', '131', '135'],
+    rightAnswer: '123',
   },
   {
     id: 5,
-    image: Model,
-    imageDescription: 'Lorem ipsum dolor sit amet',
-    question: 'Qual a soma da quantia acima?',
-    findAnswer: ['R$ 2,00', 'R$ 4,00', 'R$ 5,00', 'R$ 6,00'],
-    rightAnswer: 'R$ 5,00',
+    image: fifthQuestion,
+    imageDescription: 'Se sua compra custa R$ 120,00 e você entrega a quantia acima, quanto receberá de troco?',
+    question: 'Se sua compra custa R$ 120,00 e você entrega a quantia acima, quanto receberá de troco?',
+    findAnswer: ['R$ 15,85', 'R$ 16,15', 'R$ 16,85', 'R$ 17,15'],
+    rightAnswer: 'R$ 15,85',
   },
 ];
 
